03b39663b68a1ad8a59b13807348452ef13192b4
Hunterlemming <kklemming@gmail.com>
2022-02-22 16:32:22 +0100

    gen-1.csv

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 from typing import Dict, List, Optional
 import pandas as pd
+from model.log import LogSummary
 
 from services.log_parser import parse_default_csv
 
 
-def create_csv_1():
-    raise NotImplementedError
+def create_csv_1(log: LogSummary):
+    rows: List[List[pd.Timestamp.date, int]] = []
+
+    actor0_days = filter(lambda day: day.actor == log.actors[0], log.days)
+
+    for day in actor0_days:
+        rows.append([day.date, day.get_number_of_activities_performed()])
+    df = pd.DataFrame(rows, columns=['timestamp', 'num of activity performed'])
+    print(rows)
+    df.set_index('timestamp')
+    print(df)
+    # TODO: We need to set the first column as index
+    df.to_csv('./data/gen_1.csv', sep=';')
 
 
 if __name__ == "__main__":
     summary = parse_default_csv()
-    print(summary)
+    create_csv_1(summary)
--- model/day.py
+++ model/day.py
@@ -11,6 +11,9 @@ class Day:
         self.date: pd.Timestamp.date = date
         self.tasks: List[Task] = tasks if tasks is not None else []
 
+    def get_number_of_activities_performed(self):
+        return len(self.tasks)
+
     def __str__(self) -> str:
         _str = [f"Actor: {self.actor}| Date: {self.date}| Tasks:"]
         for task in self.tasks:
