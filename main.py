711a7f8eb5398f66e71ca4dbd000b91671bd64bd
Hunterlemming <kklemming@gmail.com>
2022-02-23 14:57:12 +0100

    gen-1.csv update, day functions p1

--- main.py
+++ main.py
@@ -1,25 +1,25 @@
-from typing import Dict, List, Optional
+from typing import List
 import pandas as pd
 from model.log import LogSummary
 
 from services.log_parser import parse_default_csv
 
 
-def create_csv_1(log: LogSummary):
+def create_csv_1(log: LogSummary, actor: str):
     rows: List[List[pd.Timestamp.date, int]] = []
 
     actor0_days = filter(lambda day: day.actor == log.actors[0], log.days)
 
     for day in actor0_days:
-        rows.append([day.date, day.get_number_of_activities_performed()])
+        rows.append([pd.to_datetime(day.date, format='%Y-%m-%d'), day.get_daily_all_number_of_products()])
     df = pd.DataFrame(rows, columns=['timestamp', 'num of activity performed'])
-    print(rows)
-    df.set_index('timestamp')
-    print(df)
-    # TODO: We need to set the first column as index
-    df.to_csv('./data/gen_1.csv', sep=';')
+    df.set_index('timestamp', inplace=True)
+    df.to_csv('./data/generated/gen_1.csv', sep=';')
 
 
 if __name__ == "__main__":
     summary = parse_default_csv()
-    create_csv_1(summary)
+    # create_csv_1(summary)
+    print(summary.days[0])
+    wh = summary.days[0].get_daily_product_per_hour_performance()
+    print(wh)
--- model/day.py
+++ model/day.py
@@ -1,7 +1,10 @@
-from typing import List, Optional
+from typing import List, Optional, Tuple
 import pandas as pd
 
 from model.task import Task
+
+
+DAY_INTERVALS: List[Tuple[str, str]] = [('08:00', '10:00'), ('10:00', '12:00'), ('12:00', '14:00'), ('14:00', '16:00')]
 
 
 class Day:
@@ -11,8 +14,35 @@ class Day:
         self.date: pd.Timestamp.date = date
         self.tasks: List[Task] = tasks if tasks is not None else []
 
-    def get_number_of_activities_performed(self):
+
+    def get_daily_all_number_of_products(self) -> int:
+        '''Returns the daily number of products.'''
         return len(self.tasks)
+
+    def get_daily_hours_worked(self) -> float:
+        '''Returns the amount of hours worked this day (with 3 decimals).'''
+        worktime: float = 0
+        # Calculating the worktime in seconds
+        for task in self.tasks:
+            tasktime: pd.Timedelta = pd.to_datetime(str(task.end)) - pd.to_datetime(str(task.start))
+            worktime += tasktime.seconds
+        # Returning the worktime in hours (3 decimals)
+        return round(worktime / 3600, 3)
+
+    def get_daily_product_per_hour_performance(self) -> float:
+        '''Returns the average number of products created in a workhour'''
+        return self.get_daily_all_number_of_products() / self.get_daily_hours_worked()
+
+
+    def get_interval_productivity(self, start: str, end: str) -> float:
+        # TODO: this
+        worktime: float = 0
+        products: int = 0
+        for task in self.tasks:
+            pass
+
+    def get_worktime_in_intervals(self) -> List[float]:
+        pass
 
     def __str__(self) -> str:
         _str = [f"Actor: {self.actor}| Date: {self.date}| Tasks:"]
